db4a8ade3860656ccc9ea9b207126a5bc894d033
Craig Perkins <cwperx@amazon.com>
2022-07-20 13:42:58 -0400

    Apply change to opendistro as well
    Signed-off-by: Craig Perkins <cwperx@amazon.com>

--- .ci/run-opensearch.sh
+++ .ci/run-opensearch.sh
@@ -45,9 +45,6 @@ END
 ))
   echo "$i: $http_port $node_url "
   volume_name=${node_name}-rest-test-data
-  if [[ "$SECURE_INTEGRATION" == "true" ]]; then
-    volume_name=${node_name}-rest-test-data-with-security
-  fi
   volumes+=($(cat <<-END
     --volume $volume_name:/usr/share/opensearch/data${i}
 END
